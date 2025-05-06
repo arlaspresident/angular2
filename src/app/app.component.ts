44a36f3ea9b0323821b2af78f3a937cf39efb57c
arlaspresident <jankovic4901@gmail.com>
2025-05-06 13:10:30 +0200

    la till tabellen med kurser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CourseTableComponent } from './components/course-table/course-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CourseTableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'kurs-tabell';
---
+++ src/app/components/course-table/course-table.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseService } from '../../services/course.service';
+import { Course } from '../../models/course.model';
+
+
+@Component({
+  selector: 'app-course-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './course-table.component.html',
+  styleUrls: ['./course-table.component.css']
+})
+export class CourseTableComponent implements OnInit{
+  courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this.courseService.getCourses().subscribe({
+      next: (data) => {
+        this.courses = data;
+        this.filteredCourses = data;
+      },
+      error: (err) => console.error(err)
+    });
+  }
+}
